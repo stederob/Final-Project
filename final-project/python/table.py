06b38ec120553f0d8c172f5906625129dddb61cc
stederob <ste.deroberto@gmail.com>
2013-07-13 15:20:51 +0200

    Update table.py

--- final-project/python/table.py
+++ final-project/python/table.py
@@ -57,8 +57,9 @@ def GRID(args):
 dom1D = INTERVALS(1)(32)
 dom2D = GRID([20,20])
 
-light_brown = COLOR([194/255, 115/255, 51/255])
-brown = COLOR([150/255, 75/255, 0/255])
+light_brown = COLOR([194.0/255, 115.0/255, 51.0/255])
+brown = COLOR([101.0/255, 67.0/255, 33.0/255])
+
 
 
 def table_top(c0,c1,c00,c11,w,h,s):
@@ -74,51 +75,40 @@ def table_top(c0,c1,c00,c11,w,h,s):
 	bez2 = MAP(BEZIER(S2)([c00,c11]))(dom2D);
 	bez3 = T([3])([h])(bez1);
 	bez4 = T([3])([h])(bez2);
-	bez5 = light_brown(T([3])([h-0.1])(MAP(BEZIER(S1)([c1,c11]))(dom2D)));
+	bez5 = light_brown(T([3])([h-0.1])(MAP(BEZIER(S2)([c1,c11]))(dom2D)));
 	border_r0 = brown(STRUCT([bez1,bez2,bez3,bez4,cmap0,cmap1,cmap00,cmap11,cuboid]))
 	border_r = STRUCT([border_r0,bez5])
 	border_l = T([1,2])([4,6.1])(R([1,2])(PI)(border_r))
 	border = STRUCT([border_r,border_l,cuboid1,cuboid2,cuboid3])
 	return border;
 
-def trasla (p,v):
-	q = []
-	length=len(p)
-	for i in range(length):
-		q += [ADD([p[i],v])]
-	return q;
 
-
-
-def table(punti0,punti1,punti2,n,w,d,s):
+def table(n,w,d,s):
 	tavoli = []
 	for i in range(n):
 		cyl = CYLINDER([0.1,6.8-i*s])(24)
-		p0 = trasla(punti0,[+i*s,+i*s,0])
-		p1 = trasla(punti1,[-i*s,+i*s,0])
-		p2 = trasla(punti2,[-i*s,+i*s,0])
+		p0 = [[0.3+i*s,0+i*s,0], [0.3+i*s,0.3+i*s,0]]
+		p1 = [[3.4-i*s,0+i*s,0],[4-i*s,0.6+i*s,0]]
+		p2 = [[3.4-i*s,0.3+i*s,0],[3.7-i*s,0.6+i*s,0]]
 		c0 = CUBICHERMITE(S1)([p0[0],p0[1],[-s*2,0,0],[s*2,0,0]]);
 		c1 = CUBICHERMITE(S1)([p1[0],p1[1],[s*4,0,0],[0,s*4,0]]);
 		c2 = CUBICHERMITE(S1)([p2[0],p2[1],[s*2,0,0],[0,s*2,0]]);
-		p00[0] = [punti0[0]+i*s,d-(punti0[1]+i*s),punti0[2]]
-		p00[1] = [punti0[0]+i*s,d-(punti0[1]+i*s),punti0[2]]
-		p11[0] = [punti1[0]-i*s,d-(punti1[1]+i*s),punti1[2]]
-		p11[1] = [punti1[0]-i*s,d-(punti1[1]+i*s),punti1[2]]
-		p22[0] = [punti2[0]-i*s,d-(punti2[1]+i*s),punti2[2]]
-		p22[1] = [punti2[0]-i*s,d-(punti2[1]+i*s),punti2[2]]
-		p3 = trasla(p0,[i*s,+i*s,+s])
-		p4 = trasla(p00,[i*s,i*s,s])
+		p00 = [[0.3 + i*s,d-(0+i*s),0],[0.3+i*s,d-(0.3+i*s),0]]
+		p11 = [[3.4-i*s,d-(0+i*s),0],[4-i*s,d-(0.6+i*s),0]]
+		p22 = [[3.4-i*s,d-(0.3+i*s),0],[3.7-i*s,d-(0.6+i*s),0]]
+		p3 = [[0.3+2*i*s,0+2*i*s,0+s], [0.3+2*i*s,0.3+2*i*s,0+s]]
+		p4 = [[0.3 + 2*i*s,(d-(0+i*s))+i*s,0+s],[0.3+i*s,(d-(0.3+i*s))+i*s,0+s]]
 		c3 = CUBICHERMITE(S1)([p00[0],p00[1],[-s*2,0,0],[s*2,0,0]]);
 		c4 = CUBICHERMITE(S1)([p11[0],p11[1],[s*4,0,0],[0,-s*4,0]]);
 		c5 = CUBICHERMITE(S1)([p22[0],p22[1],[s*2,0,0],[0,-s*2,0]]);
 		c00 = BEZIER(S1)(p0);
 		c33 = BEZIER(S1)(p00);
-		cmap0 = MAP(CYLINDRICALSURFACE(c0,[0,0,s]))(dom2D);
-		cmap1 = MAP(CYLINDRICALSURFACE(c1,[0,0,s]))(dom2D);
-		cmap2 = MAP(CYLINDRICALSURFACE(c2,[0,0,s]))(dom2D);
-		cmap3 = MAP(CYLINDRICALSURFACE(c3,[0,0,s]))(dom2D);
-		cmap4 = MAP(CYLINDRICALSURFACE(c4,[0,0,s]))(dom2D);
-		cmap5 = MAP(CYLINDRICALSURFACE(c5,[0,0,s]))(dom2D);
+		cmap0 = MAP(CYLINDRICALSURFACE([c0,[0,0,s]]))(dom2D);
+		cmap1 = MAP(CYLINDRICALSURFACE([c1,[0,0,s]]))(dom2D);
+		cmap2 = MAP(CYLINDRICALSURFACE([c2,[0,0,s]]))(dom2D);
+		cmap3 = MAP(CYLINDRICALSURFACE([c3,[0,0,s]]))(dom2D);
+		cmap4 = MAP(CYLINDRICALSURFACE([c4,[0,0,s]]))(dom2D);
+		cmap5 = MAP(CYLINDRICALSURFACE([c5,[0,0,s]]))(dom2D);
 		bez1 = MAP(BEZIER(S2)([c1,c2]))(dom2D);
 		bez2 = MAP(BEZIER(S2)([c4,c5]))(dom2D);
 		bez3 = T([3])([s])(bez1);
@@ -150,35 +140,30 @@ def table(punti0,punti1,punti2,n,w,d,s):
 			cuboid4 = brown(T([2])([p00[0][1]-i*s-s-0.05])(cuboid3))
 			cuboid5 = brown(T([1,2,3])([p1[0][0]-p0[0][0]+i*s+s,i*s+0.15,5.9])(CUBOID([0.2,p11[0][1]-p1[0][1]-0.2,0.9])))
 			cuboid6 = T([3])([6.8])(table_top(c1,c2,c4,c5,w,0.6,s))
+		
 		cub = STRUCT([cuboid3,cuboid4,cuboid5,cuboid6])
 		table = STRUCT([basis,table_leg,cub])
-		#tavoli += [table]
-	return cuboid6;
+		tavoli += [table]
+	return tavoli;
 
-p1 = [[0.3,0,0],[0.3,0.3,0]]
-p2 = [[3.4,0,0],[4,0.6,0]]
-p3 = [[3.4,0.3,0],[3.7,0.6,0]]
-t = table(p1,p2,p3,4,4,6.1,0.3)
+t = table(4,4,6.1,0.3)
 
 function sphere(r){
 	return function(v){
 	return [r*SIN(v[0])*COS(v[1]),r*SIN(v[0])*SIN(v[1]),r*COS(v[0])]}
 }
 
-function spheres(r,n){
-	var domain_sfera = DOMAIN([[0,2*PI],[0,2*PI]])([24,36]);
-	var mapping = sphere(r);
-	var model = MAP(mapping)(domain_sfera);
-	var model1 = model;
-	for (i=1; i<n;i++){
-		s = T([0])([i*2*r])(model)
+def spheres(r,n):
+	model = SPHERE(r)([24,36])
+	model1 = model
+	for i in range(n-1):
+		s = T([1])([(i+1)*2*r])(model)
 		model1 = STRUCT([s,model1])
-	}
 	return brown(model1);
-}
-var spheres_r= T([0,1,2])([1.4,-0.2,7.1])(spheres(0.2,4))
-var spheres_l = T([0,1,2])([1.4,6.3,7.1])(spheres(0.2,4))
-var sp = STRUCT([spheres_l,spheres_r])
-var table = STRUCT([t[0],t[1],t[2],t[3],sp])
-DRAW(table)
+
+spheres_r= T([1,2,3])([1.4,-0.2,7.1])(spheres(0.2,4))
+spheres_l = T([1,2,3])([1.4,6.3,7.1])(spheres(0.2,4))
+sp = STRUCT([spheres_l,spheres_r])
+table = STRUCT([t[0],T([1])([-1.5])(t[1]),T([1])([-2.5])(t[2]),T([1])([-4])(t[3]),sp])
+VIEW(table)
 
