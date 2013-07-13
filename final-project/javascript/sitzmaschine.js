5a6fb2348a03c29f7b65b137a8f30a60eba298b2
stederob <ste.deroberto@gmail.com>
2013-07-13 15:16:42 +0200

    Update sitzmaschine.js

--- final-project/javascript/sitzmaschine.js
+++ final-project/javascript/sitzmaschine.js
@@ -1,6 +1,8 @@
-//cornicioni
+//lateral side
 var domain = PROD1x1([INTERVALS(1)(14),INTERVALS(1)(14)]);
 var dom = INTERVALS(1)(14)
+var brown = COLOR([101.0/255, 67.0/255, 33.0/255])
+
 var c1 = CUBIC_HERMITE(S0)([[5,5.5,5],[4.85,8,2.5],[0,4,0],[0,0,-4]]);
 var c2 = CUBIC_HERMITE(S0)([[4.85,8,2.5],[4.7,5.5,0],[0,0,-4],[0,-4,0]]);
 var c3 = CUBIC_HERMITE(S0)([[5,5.5,4.8],[4.85,7.8,2.5],[0,3.4,0],[0,0,-3.4]]);
@@ -20,7 +22,7 @@ var out6 = MAP(BEZIER(S1)([c2,c4]))(domain);
 var out7 = MAP(BEZIER(S1)([c11,c33]))(domain);
 var out8 = MAP(BEZIER(S1)([c22,c44]))(domain);
 
-s = STRUCT ([out1,out2,out3,out4,out5,out6,out7,out8])
+var s = STRUCT ([out1,out2,out3,out4,out5,out6,out7,out8])
 
 var c1b = CUBIC_HERMITE(S0)([[-0.5,5.5,5],[-0.35,8,2.5],[0,4,0],[0,0,-4]]);
 var c2b = CUBIC_HERMITE(S0)([[-0.35,8,2.5],[-0.2,5.5,0],[0,0,-4],[0,-4,0]]);
@@ -41,32 +43,32 @@ var out6b = MAP(BEZIER(S1)([c2b,c4b]))(domain);
 var out7b = MAP(BEZIER(S1)([c11b,c33b]))(domain);
 var out8b = MAP(BEZIER(S1)([c22b,c44b]))(domain);
 
-b = STRUCT ([out1b,out2b,out3b,out4b,out5b,out6b,out7b,out8b])
+var b = STRUCT ([out1b,out2b,out3b,out4b,out5b,out6b,out7b,out8b])
 
-cuboid = CUBOID([0.5,5.5,0.2])
-ct = T([0,2])([4.5,4.8])(cuboid)
-ctb = T([0,2])([-0.5,4.8])(cuboid)
+var cuboid = CUBOID([0.5,5.5,0.2])
+var ct = T([0,2])([4.5,4.8])(cuboid)
+var ctb = T([0,2])([-0.5,4.8])(cuboid)
 
-cuboid1 = CUBOID([0.2,4.95,0.2])
-ct1 = T([0,1])([4.5,0.55])(cuboid1)
-ct1b = T([0,1])([-0.2,0.55])(cuboid1)
+var cuboid1 = CUBOID([0.2,4.95,0.2])
+var ct1 = T([0,1])([4.5,0.55])(cuboid1)
+var ct1b = T([0,1])([-0.2,0.55])(cuboid1)
 
 var c5 = CUBIC_HERMITE(S0)([[4.7,0.15,0.4],[4.7,0.55,0],[0,0,-0.6],[0,0.6,0]]);
 var c6 = CUBIC_HERMITE(S0)([[4.7,0.35,0.4],[4.7,0.55,0.2],[0,0,-0.2],[0,0.2,0]]);
-out9 = MAP(c5)(dom)
-out10 = MAP(c6)(dom)
+var out9 = MAP(c5)(dom)
+var out10 = MAP(c6)(dom)
 
 var out9 = MAP(CYLINDRICAL_SURFACE(c5)([-0.2,0,0]))(domain);
 var out10 = MAP(CYLINDRICAL_SURFACE(c6)([-0.2,0,0]))(domain);
 var out11 = MAP(BEZIER(S1)([c5,c6]))(domain);
 var out12 = T([0])([-0.2])(out11);
-s1 = STRUCT([out9,out10,out11,out12])
+var s1 = STRUCT([out9,out10,out11,out12])
 
-cuboid2 = CUBOID([0.2,0.2,4.4])
-ct2 = T([0,1,2])([4.5,0.15,0.4])(cuboid2)
+var cuboid2 = CUBOID([0.2,0.2,4.4])
+var ct2 = T([0,1,2])([4.5,0.15,0.4])(cuboid2)
 
-cs = STRUCT([ct2,s1,ct1])
-csb = T([0])([-4.7])(cs)
+var cs = STRUCT([ct2,s1,ct1])
+var csb = T([0])([-4.7])(cs)
 
 //sfera
 var domain_sfera = DOMAIN([[0,2*PI],[0,2*PI]])([24,36]);
@@ -82,7 +84,7 @@ var mt1 = T([0,1,2])([-0.35,0.23,4.6])(model)
 var mt2 = T([0,1,2])([4.25,0.23,2.1])(model)
 var mt3 = T([0,1,2])([0.2,0.23,2.1])(model)
 
-struct = COLOR([184/255, 115/255, 51/255])(STRUCT([cs,csb,ctb,ct,s,b,mt,mt1,mt2,mt3]))
+var struct = brown(STRUCT([cs,csb,ctb,ct,s,b,mt,mt1,mt2,mt3]))
 
 
 //rifinitura cornicione
@@ -103,10 +105,10 @@ function wall_y_with_N_hole(dx,dy,dz,hole_y,hole_z,hole_dy,hole_dz,n){
 	return t;
 }
 
-f1 = wall_y_with_N_hole(0.1,0.4,3,0.1,1,0.2,1.5,5)
-f2 = wall_y_with_N_hole(0.1,0.4,1.4,0.1,0.5,0.2,0.2,5)
-f1t = T([0,1,2])([-0.15,2.2,1.7])(f1)
-f2t = T([0,1,2])([-0.15,2.2,0.3])(f2)
+var f1 = wall_y_with_N_hole(0.1,0.4,3,0.1,1,0.2,1.5,5)
+var f2 = wall_y_with_N_hole(0.1,0.4,1.4,0.1,0.5,0.2,0.2,5)
+var f1t = T([0,1,2])([-0.15,2.2,1.7])(f1)
+var f2t = T([0,1,2])([-0.15,2.2,0.3])(f2)
 
 var domain = PROD1x1([INTERVALS(1)(14),INTERVALS(1)(14)]);
 var dom = INTERVALS(1)(14)
@@ -150,11 +152,11 @@ var cx2 = CUBOID([0.2,3,0.1])
 var cx2t = T([0,1,2])([-0.2,1.7,0.2])(cx2)
 var cx2tb = T([0,1,2])([-0.2,1.7,4.7])(cx2)
 
-struct_rif = STRUCT([rif,srt,cx2t,cx2tb,f1t,f2t])
-rif2 = T([0])([4.7])(struct_rif)
-rifinitura = STRUCT([struct_rif,rif2])
-
-sedia = COLOR([184/255, 115/255, 51/255])(STRUCT([rifinitura,struct]))
+var struct_rif = STRUCT([rif,srt,cx2t,cx2tb,f1t,f2t])
+var rif2 = T([0])([4.7])(struct_rif)
+var rifinitura = STRUCT([struct_rif,rif2])
+ 
+var sedia = brown(STRUCT([rifinitura,struct]))
 
 //schienale
 var p2 = [[0,5.5,3],[0.4,5.5,2.6],[0,0,-0.8],[0.8,0,0]]
@@ -202,11 +204,11 @@ var interno1 = SIMPLEX_GRID([[-1.05,2.4],[-5.575,0.05],
 var interno2 = SIMPLEX_GRID([[-1.05,0.5,-0.5,0.4,-0.5,0.5],[-5.575,0.05],[-2.8,7.1]]) 
 var schienale_int = STRUCT([sc1,scr1,cil3,cil3t,interno1,interno2])
 
-var schienale = COLOR([184/255, 115/255, 51/255])(T([1,2])([-0.4,2.1])(R([1,2])([-PI/9])(STRUCT([schienale_int,schienale_ext]))))
+var schienale = brown(T([1,2])([-0.4,2.1])(R([1,2])([-PI/9])(STRUCT([schienale_int,schienale_ext]))))
 
 //sedile
-sedile = CUBOID([4.5,5.5,0.3])
-sed_t = T([2])([2.3])(sedile)
+var sedile = CUBOID([4.5,5.5,0.3])
+var sed_t = T([2])([2.3])(sedile)
 
 var l1 = CUBIC_HERMITE(S0)([[0,0.8,2.3],[0.8,0,2.3],[0,-1.6,0],[1.6,0,0]]);
 var l2 = CUBIC_HERMITE(S0)([[0,4.7,2.3],[0.8,5.5,2.3],[0,1.6,0],[1.6,0,0]]);
@@ -222,52 +224,52 @@ var s = STRUCT([v12,v21,var1l,var2l,var3l]);
 var srt = T([0,1])([4.5,5.5])(R([0,1])(PI)(s))
 var centro = T([0,2])([0.8,2.3])(CUBOID([2.9,5.5,0.3]))
 
-var sedile = COLOR([184/255, 115/255, 51/255])(STRUCT([centro, srt,s]))
+var sedile = brown(STRUCT([centro, srt,s]))
 
 
-//funghetti
+//knob
 function cylinder(r,h){
 	d = DISK(r)(36)
 	c = EXTRUDE([h])(d)
 	return c
 }
 
-var domain_fung = DOMAIN([[-PI/2, PI/2],[-PI/2,PI/2]])([24,36]);
+var domain_kb = DOMAIN([[-PI/2, PI/2],[-PI/2,PI/2]])([24,36]);
 var mapping = sfera(0.15);
-var model1 = T([2])([0.1])(MAP(mapping)(domain_fung));
+var model1 = T([2])([0.1])(MAP(mapping)(domain_kb));
 var d = cylinder(0.1,0.1);
 var d1 = T([2])([0.1])(DISK(0.15)(24,36));
-var fung = STRUCT([model1,d,d1]);
+var kb = STRUCT([model1,d,d1]);
 
 
-dt1 = T([0,1,2])([-0.25,5.5,5])(fung)
-dt2 = T([1,2])([0.1,0.75])(R([1,2])(-PI/20)(dt1))
-dt3 = T([0,1,2])([0.02,0.2,1.4])(R([1,2])(-2*PI/20)(dt1))
-dt4 = T([0,1,2])([0.03,0.285,2])(R([1,2])(-3*PI/20)(dt1))
-dt5 = T([0,1,2])([0.04,0.45,2.45])(R([1,2])(-4*PI/20)(dt1))
-funghetti_left = STRUCT([dt1,dt2,dt3,dt4,dt5])
-funghetti_right = T([0])([4.95])(funghetti_left)
+var dt1 = T([0,1,2])([-0.25,5.5,5])(kb)
+var dt2 = T([1,2])([0.1,0.75])(R([1,2])(-PI/20)(dt1))
+var dt3 = T([0,1,2])([0.02,0.2,1.4])(R([1,2])(-2*PI/20)(dt1))
+var dt4 = T([0,1,2])([0.03,0.285,2])(R([1,2])(-3*PI/20)(dt1))
+var dt5 = T([0,1,2])([0.04,0.45,2.45])(R([1,2])(-4*PI/20)(dt1))
+var knob_left = STRUCT([dt1,dt2,dt3,dt4,dt5])
+var knob_right = T([0])([4.95])(knob_left)
 
-funghetti = COLOR([184/255, 115/255, 51/255])(STRUCT([funghetti_left,funghetti_right]))
+var knob = brown(STRUCT([knob_left,knob_right]))
 
-cilindro = cylinder(0.07,6)
-cilindro_r = R([0,2])(PI/2)(cilindro)
-cilindro_t = COLOR([0,0,0])(T([0,1,2])([-0.7,6.8,4.6])(cilindro_r))
-sfera1 = COLOR([184/255, 115/255, 51/255])(T([0,1,2])([-0.7,6.8,4.6])(model))
-sfera2 = COLOR([184/255, 115/255, 51/255])(T([0,1,2])([5.3,6.8,4.6])(model))
-cil_sf = STRUCT([cilindro_t,sfera2,sfera1])
+var cilindro = cylinder(0.07,6)
+var cilindro_r = R([0,2])(PI/2)(cilindro)
+var cilindro_t = COLOR([0,0,0])(T([0,1,2])([-0.7,6.8,4.6])(cilindro_r))
+var sfera1 = brown(T([0,1,2])([-0.7,6.8,4.6])(model))
+var sfera2 = brown(T([0,1,2])([5.3,6.8,4.6])(model))
+var cil_sf = STRUCT([cilindro_t,sfera2,sfera1])
 
 //cerniera
-cil4 = cylinder(0.1,0.6)
-cil4r = R([0,2])([PI/2])(cil4)
-cil4t1 = T([0,1,2])([0.8,5.6,2.65])(cil4r)
-cil4t2 = T([0,1,2])([3.1,5.6,2.65])(cil4r)
-cerniera = COLOR([0.3,0.3,0.3])(STRUCT([cil4t2,cil4t1]))
+var cil4 = cylinder(0.1,0.6)
+var cil4r = R([0,2])([PI/2])(cil4)
+var cil4t1 = T([0,1,2])([0.8,5.6,2.65])(cil4r)
+var cil4t2 = T([0,1,2])([3.1,5.6,2.65])(cil4r)
+var cerniera = COLOR([0.3,0.3,0.3])(STRUCT([cil4t2,cil4t1]))
 
 //ruote
 var domain_ruota = DOMAIN([[0,PI/2],[3*PI/8,5*PI/8]])([24,36]);
 var mapping1 = sfera(0.3);
-var model1 = COLOR([184/255, 115/255, 51/255])(MAP(mapping1)(domain_ruota));
+var model1 = brown(MAP(mapping1)(domain_ruota));
 
 var disk = DISK(0.15)(24)
 var disk1 = EXTRUDE([0.2])(disk)
@@ -277,5 +279,5 @@ var ruota = STRUCT([COLOR([0.3,0.3,0.3])(disk_t),model1])
 var ruota1 = T([0,1])([-0.1,6.5])(ruota)
 var ruota2 = T([0,1])([4.6,6.5])(ruota)
 var ruote = STRUCT([ruota2,ruota1])
-var sedia = STRUCT([sedia,sedile,schienale,funghetti,cil_sf,cerniera,ruote])
+var sedia = STRUCT([sedia,sedile,schienale,knob,cil_sf,cerniera,ruote])
 DRAW(sedia)
